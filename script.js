e20961f93b1bcd6ffac0874a826fd8df667418f5
Parampreet <parampreet.mvteams@gmail.com>
2026-05-26 17:57:37 +0530

    add some new files

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const swiper1 = new Swiper(".mySwiper", {
     loop: true,
+    spaceBetween: 10,
       navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
